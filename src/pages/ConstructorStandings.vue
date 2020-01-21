a2ea16125cca7a70569e0258d2bd9838d749e23d
Alex Gouws <alex.gouws@gmail.com>
2020-01-21 06:11:11 +0200

    Fixed Red Bull team image not displaying

--- src/pages/ConstructorStandings.vue
+++ src/pages/ConstructorStandings.vue
@@ -1,244 +1,259 @@
 <template>
-<q-page v-show="!isLoading">
-  <div class="constructor">
-    <div class="teamcontainer">
-      <div class="team">
-        <div class="second-constructor">
-          <q-card class="fill" flat square>
-            <q-img :src="getTeamImage(1)">
-            </q-img>
-            <q-card-section>
-              <div class="row">
-                <div class="col text-center">
-                  <q-chip class="second-constructor-badge">
-                    <q-avatar>
-                      <img src="~assets/medals/silver.png" width="24" height="24">
-                    </q-avatar>
-                    <div class="text-bold">{{ teams[1].position }}nd</div>
-                  </q-chip>
+  <q-page v-show="!isLoading">
+    <div class="constructor">
+      <div class="teamcontainer">
+        <div class="team">
+          <div class="second-constructor">
+            <q-card class="fill" flat square>
+              <q-img :src="getTeamImage(1)"></q-img>
+              <q-card-section>
+                <div class="row">
+                  <div class="col text-center">
+                    <q-chip class="second-constructor-badge">
+                      <q-avatar>
+                        <img
+                          src="~assets/medals/silver.png"
+                          width="24"
+                          height="24"
+                        />
+                      </q-avatar>
+                      <div class="text-bold">{{ teams[1].position }}nd</div>
+                    </q-chip>
+                  </div>
                 </div>
-              </div>
-              <div class="text-subtitle2 text-center">{{ teams[1].name }}</div>
-              <hr>
-              <div class="text-center">
-                <div>{{ teams[1].wins }} Wins</div>
-                <div>{{ teams[1].points }} Points</div>
-              </div>
-            </q-card-section>
-          </q-card>
-        </div>
+                <div class="text-subtitle2 text-center">
+                  {{ teams[1].name }}
+                </div>
+                <hr />
+                <div class="text-center">
+                  <div>{{ teams[1].wins }} Wins</div>
+                  <div>{{ teams[1].points }} Points</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
 
-        <div class="first-constructor">
-          <q-card class="fill" flat square>
-            <q-img :src="getTeamImage(0)">
-            </q-img>
-            <q-card-section>
-              <div class="row">
-                <div class="col text-center">
-                  <q-chip class="first-constructor-badge">
-                    <q-avatar>
-                      <img src="~assets/medals/gold.png" width="24" height="24">
-                    </q-avatar>
-                    <div class="text-bold">{{ teams[0].position }}st</div>
-                  </q-chip>
+          <div class="first-constructor">
+            <q-card class="fill" flat square>
+              <q-img :src="getTeamImage(0)"></q-img>
+              <q-card-section>
+                <div class="row">
+                  <div class="col text-center">
+                    <q-chip class="first-constructor-badge">
+                      <q-avatar>
+                        <img
+                          src="~assets/medals/gold.png"
+                          width="24"
+                          height="24"
+                        />
+                      </q-avatar>
+                      <div class="text-bold">{{ teams[0].position }}st</div>
+                    </q-chip>
+                  </div>
                 </div>
-              </div>
-              <div class="text-subtitle2 text-center">{{ teams[0].name }}</div>
-              <hr>
-              <div class="text-center">
-                <div>{{ teams[0].wins }} Wins</div>
-                <div>{{ teams[0].points }} Points</div>
-              </div>
-            </q-card-section>
-          </q-card>
-        </div>
+                <div class="text-subtitle2 text-center">
+                  {{ teams[0].name }}
+                </div>
+                <hr />
+                <div class="text-center">
+                  <div>{{ teams[0].wins }} Wins</div>
+                  <div>{{ teams[0].points }} Points</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
 
-        <div class="third-constructor">
-          <q-card class="fill" flat square>
-            <q-img :src="getTeamImage(2)">
-            </q-img>
-            <q-card-section>
-              <div class="row">
-                <div class="col text-center">
-                  <q-chip class="third-constructor-badge">
-                    <q-avatar>
-                      <img src="~assets/medals/bronze.png" width="24" height="24">
-                    </q-avatar>
-                    <div class="text-bold">{{ teams[2].position }}rd</div>
-                  </q-chip>
+          <div class="third-constructor">
+            <q-card class="fill" flat square>
+              <q-img :src="getTeamImage(2)"></q-img>
+              <q-card-section>
+                <div class="row">
+                  <div class="col text-center">
+                    <q-chip class="third-constructor-badge">
+                      <q-avatar>
+                        <img
+                          src="~assets/medals/bronze.png"
+                          width="24"
+                          height="24"
+                        />
+                      </q-avatar>
+                      <div class="text-bold">{{ teams[2].position }}rd</div>
+                    </q-chip>
+                  </div>
                 </div>
-              </div>
-              <div class="text-subtitle2 text-center">{{ teams[2].name }}</div>
-              <hr>
-              <div class="text-center">
-                <div>{{ teams[2].wins }} Wins</div>
-                <div>{{ teams[2].points }} Points</div>
-              </div>
-            </q-card-section>
-          </q-card>
+                <div class="text-subtitle2 text-center">
+                  {{ teams[2].name }}
+                </div>
+                <hr />
+                <div class="text-center">
+                  <div>{{ teams[2].wins }} Wins</div>
+                  <div>{{ teams[2].points }} Points</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="constructor-header-container"
+        id="constructor-header-container"
+      >
+        <div class="header-position dark">Pos</div>
+        <div class="header">Team Name</div>
+        <div class="header">Wins</div>
+        <div class="header">Points</div>
+      </div>
+
+      <div
+        v-for="(team, index) in teams.slice(3)"
+        :key="index"
+        class="flex-container"
+      >
+        <div class="position">
+          <q-item-label>{{ team.position }}</q-item-label>
+        </div>
+        <div class="item">
+          <q-item-section>
+            <q-item-label class="text-center">{{ team.name }}</q-item-label>
+          </q-item-section>
+        </div>
+        <div class="item">{{ team.wins }}</div>
+        <div class="item">
+          <q-item-label>{{ team.points }}</q-item-label>
         </div>
       </div>
     </div>
-
-    <div class="constructor-header-container" id="constructor-header-container">
-      <div class="header-position dark">
-        Pos
-      </div>
-      <div class="header">
-        Team Name
-      </div>
-      <div class="header">
-        Wins
-      </div>
-      <div class="header">
-        Points
-      </div>
-    </div>
-
-    <div v-for="(team, index) in teams.slice(3)" :key="index" class="flex-container">
-      <div class="position">
-          <q-item-label>{{team.position}}</q-item-label>
-      </div>
-      <div class="item">
-        <q-item-section>
-          <q-item-label class="text-center">{{team.name}}</q-item-label>
-        </q-item-section>
-      </div>
-      <div class="item">
-        {{team.wins}}
-      </div>
-      <div class="item">
-        <q-item-label>{{team.points}}</q-item-label>
-      </div>
-    </div>
-
-  </div>
-</q-page>
+  </q-page>
 </template>
 
 <script>
-import axios from 'axios';
-import JQuery from 'jquery';
+import axios from "axios";
+import JQuery from "jquery";
 let $ = JQuery;
 
 export default {
-  name: 'constructors',
-  data () {
+  name: "constructors",
+  data() {
     return {
       isLoading: true,
-      teams: [],
-    }
+      teams: []
+    };
   },
   created() {
-    this.getData();window.addEventListener('scroll', this.handleScroll);
-    setTimeout(function(){
-        window.scrollTo(0, 0); 
-    }, 1500); 
+    this.getData();
+    window.addEventListener("scroll", this.handleScroll);
+    setTimeout(function() {
+      window.scrollTo(0, 0);
+    }, 1500);
   },
-  beforeMount(){
-    
-  },
+  beforeMount() {},
   methods: {
-    handleScroll(){
-      var sticky1 = $('#constructor-header-container');
+    handleScroll() {
+      var sticky1 = $("#constructor-header-container");
       var topOffset1 = sticky1.offset().top;
-      var scrollHeight = $(window).scrollTop()
-      if(topOffset1 - 50 <= scrollHeight){
-          sticky1.css({
-              position: "sticky",
-              top: 50,
-          });
+      var scrollHeight = $(window).scrollTop();
+      if (topOffset1 - 50 <= scrollHeight) {
+        sticky1.css({
+          position: "sticky",
+          top: 50
+        });
       }
     },
     getTeamImage(index) {
-      return `https://www.formula1.com/content/fom-website/en/teams/${this.teams[index].name}/_jcr_content/logo.img.jpg/1515152671364.jpg`
+      if (this.teams[index].name == "Red-Bull") {
+        return `https://www.formula1.com/content/fom-website/en/teams/Red-Bull-Racing/_jcr_content/logo.img.jpg/1515152671364.jpg`;
+      }
+      return `https://www.formula1.com/content/fom-website/en/teams/${this.teams[index].name}/_jcr_content/logo.img.jpg/1515152671364.jpg`;
     },
-    getData(){
+    getData() {
       this.teams = [];
-      axios.get('http://ergast.com/api/f1/current/constructorStandings.json')
-      .then(response => {
-        let res = response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
-        res.forEach((team) => {
-            let teamName = team.Constructor.name.replace(" ", "-")
+      axios
+        .get("http://ergast.com/api/f1/current/constructorStandings.json")
+        .then(response => {
+          let res =
+            response.data.MRData.StandingsTable.StandingsLists[0]
+              .ConstructorStandings;
+          res.forEach(team => {
+            let teamName = team.Constructor.name.replace(" ", "-");
             let teamStats = {
               name: teamName,
               position: team.positionText,
               wins: team.wins,
               points: team.points
-            }
+            };
             this.teams.push(teamStats);
             this.isLoading = false;
+          });
+        })
+        .catch(e => {
+          console.log(e);
         });
-      })
-      .catch(e => {
-        console.log(e);
-      })
     }
-  },
-}
+  }
+};
 </script>
 
-<style >
-.constructor{
-    height: 100vh;
-    background-color: black;
+<style>
+.constructor {
+  height: 100vh;
+  background-color: black;
 }
-.teamcontainer{
+.teamcontainer {
   display: flex;
   flex-direction: row;
   background-color: black;
 }
-.team{
-    display: flex;
-    justify-content: center;
-    width: 100%;
+.team {
+  display: flex;
+  justify-content: center;
+  width: 100%;
 }
-.first-constructor{
-    height: 100%;
-    width: 100%;
+.first-constructor {
+  height: 100%;
+  width: 100%;
 }
-.second-constructor{
-    padding-top: 5%;
-    height: 100%;
-    width: 100%;
+.second-constructor {
+  padding-top: 5%;
+  height: 100%;
+  width: 100%;
 }
-.third-constructor{
-    padding-top: 5%;
-    height: 100%;
-    width: 100%;
+.third-constructor {
+  padding-top: 5%;
+  height: 100%;
+  width: 100%;
 }
 
-.fill{
-  height:100%;
+.fill {
+  height: 100%;
   background-color: rgb(44, 44, 44);
   color: white;
   white-space: nowrap;
 }
-.first-constructor-badge{
+.first-constructor-badge {
   background: gold;
 }
-.second-constructor-badge{
+.second-constructor-badge {
   background: silver;
 }
-.third-constructor-badge{
+.third-constructor-badge {
   background: #cd7f32;
 }
 
-.flex-container{
+.flex-container {
   display: flex;
   flex-direction: row;
   background-color: rgb(44, 44, 44);
-  
 }
-.item{
+.item {
   display: flex;
   justify-content: center;
   width: 100%;
-  color:white;
+  color: white;
   font-weight: 500;
   height: 50px;
 }
-.constructor-header-container{
+.constructor-header-container {
   display: flex;
   flex-direction: row;
   background-color: black;
@@ -248,25 +263,25 @@ export default {
   border-bottom: 2px solid red;
   z-index: 1;
 }
-.header{
+.header {
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
 }
-.header-position{
+.header-position {
   display: flex;
   justify-content: center;
   align-items: center;
   width: 200px;
 }
 
-.position{
+.position {
   display: flex;
   justify-content: center;
   align-items: center;
   width: 200px;
-  color:white;
+  color: white;
   background-color: #ffa200;
 }
 .centered {
@@ -275,5 +290,4 @@ export default {
   left: 50%;
   transform: translate(-50%, -50%);
 }
-
 </style>
